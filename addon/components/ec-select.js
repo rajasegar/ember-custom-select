5acb19a5dd1fa1493749d54f710920ef4c60946c
Rajasegar Chandran <rajasegar.c@gmail.com>
2017-07-20 06:24:19 +0530

    First Beta release

---
+++ addon/components/ec-select.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+import layout from '../templates/components/ec-select';
+
+const {
+  Component,
+  A
+} = Ember;
+
+export default Component.extend({
+  layout,
+  classNames: ['cs-select', 'cs-skin-border'],
+  classNameBindings: ['csActive'],
+  csActive: false,
+  children: A(),
+  actions: {
+    toggleSelect() {
+      this.toggleProperty('csActive');
+    }
+  },
+  didInsertElement() {
+    this._super(...arguments);
+    let _children = this.get('children');
+    this.$('option').each((index, el) => {
+      _children.pushObject(el.value);
+    });
+    this.set('children', _children);
+  }
+});
---
+++ app/components/ec-select.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-custom-select/components/ec-select';
